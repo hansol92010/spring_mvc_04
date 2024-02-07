567637bb7ca9ba84432fedf960df2bf86c33b980
hansol <hansol92010@gmail.com>
2024-02-07 21:11:28 +0900

    SpringMVC04

---
+++ src/main/java/kr/board/entity/Member.java
@@ -0,0 +1,15 @@
+package kr.board.entity;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private int memIdx;				// 회원 번호
+	private String memID;			// 회원 아이디
+	private String memPassword;		// 회원 비밀번호
+	private String memName;			// 회원 이름
+	private int memAge;				// 회원 나이
+	private String memGender;		// 회원 성별
+	private String memEmail;		// 회원 이메일
+	private String memProfile;		// 회원 사진
+}
